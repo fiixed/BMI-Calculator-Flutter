eca265267705c5d6cbc1bc2eea7dfa225a019332
Andrew Bell <fiixed@gmail.com>
2019-09-28 13:11:30 -0500

    navigations

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -4,6 +4,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import './icon_content.dart';
 import './reusable_card.dart';
 import './constants.dart';
+import './results_page.dart';
 
 enum Gender {
   male,
@@ -209,11 +210,22 @@ class _InputPageState extends State<InputPage> {
               ],
             ),
           ),
-          Container(
-            color: kBottomContainerColor,
-            margin: EdgeInsets.only(top: 10.0),
-            width: double.infinity,
-            height: kBottomContainerHeight,
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ResultsPage(),
+                ),
+              );
+            },
+            child: Container(
+              child: Text('CALCULATE'),
+              color: kBottomContainerColor,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: kBottomContainerHeight,
+            ),
           ),
         ],
       ),
---
+++ lib/results_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ResultsPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('BMI Calculator'),
+        ),
+        body: Text('Hello'),
+      ),
+    );
+  }
+}
